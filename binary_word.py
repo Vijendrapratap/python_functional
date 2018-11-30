3a3cd757d9e684fa0cd88e3f7dc3bfdd16d803c4
vijendra singh <adroit.k10@gmail.com>
2018-11-30 18:00:32 +0530

    This is binary search function to find the word in our list

---
+++ binary_word.py
@@ -0,0 +1,43 @@
+
+
+#difining a binary function
+def binarySearch (lst, l, r, w): 
+	
+	#sorting our list as we always need a sorted list
+	lst.sort()
+	
+	 
+	if r >= l: 
+
+		#finding mid index of our list
+		mid = (r + l)/2
+
+
+		if lst[mid] == w: 
+			return mid 
+		
+		#if our word lies between mid point and lower bond we use recursive function and also change the index of range
+		elif lst[mid] > w: 
+			return binarySearch(lst, l, mid-1, w) 
+
+
+		#if our word lies between mid point and range we use recursive function and also change the index of lower bond
+		else: 
+			return binarySearch(lst, mid+1, r, w) 
+
+	else: 
+	 
+		return -1
+
+
+lst = ["a", "quick", "brown","fox", "jumps", "over", "the", "lazy", "dog"] 
+find_word = raw_input("Enter the word you wanna search : ")
+
+# Function call 
+result = binarySearch(lst, 0, len(lst)-1, find_word) 
+
+if result != -1: 
+	print "Element is present at index %d" % result 
+else: 
+	print "Element is not present in list"
+
